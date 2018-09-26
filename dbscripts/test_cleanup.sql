1ea74065d533e4461c5104e80464027f5f0c821a
Caroline Roig-Irwin <clr2176@columbia.edu>
2018-09-26 19:56:58 -0400

    finished DAO unit tests for RDB

--- dbscripts/test_cleanup.sql
+++ dbscripts/test_cleanup.sql
@@ -1,5 +1,4 @@
 use social_customers;
-delete from customers where customers_firstname = "Angus";
+delete from customers where customers_firstname in ("Angus", "Suarez", "Lionel");
 update customers set customers_status = "DELETED" where customers_firstname = "Matt";
-
-
+update customers set customers_status = "ACTIVE" where customers_firstname = "Kate";
