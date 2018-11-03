30ad7af31dcbc1139f6c59b9f88e7bfe18713736
Caroline Roig-Irwin <clr2176@columbia.edu>
2018-11-03 16:16:02 -0400

    file reorganization. BO tests do not work at all (see issues section

---
+++ microservice/unit_tests/testorderbo.js
@@ -0,0 +1,40 @@
+/**
+ * Created by donaldferguson on 8/27/18.
+ */
+
+const logging = require('../lib/logging');
+const obo = require('../resources/orders/ordersbo');
+
+let context = { tenant: "E6156"};
+
+let testB1 = function() {
+    obo.retrieveById('sulu1', ['id'], context).then(
+        function(result) {
+            logging.debug_message("result = ", result);
+        })
+        .catch(
+            function(error) {
+                logging.error_message("error = " + error);
+            }
+        );
+};
+
+let test_create = {
+    customer: 'sulu1',
+    items: ['item1', 'item2']
+};
+
+let testB2 = function(d) {
+    obo.create(d, context).then(
+        function(result) {
+            logging.debug_message("result = ", result);
+        })
+        .catch(
+            function(error) {
+                logging.error_message("error = " + error);
+            }
+        );
+};
+
+testB1();
+//testB2(test_create);
